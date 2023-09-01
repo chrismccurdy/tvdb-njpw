da2951790e83b4790921c1c4e4fecc087bc9fd8d
Christopher McCurdy <christopher.mccurdy@gmail.com>
2023-09-01 20:49:04 +0000

    fixed episode list bug
    Was throwing an error when only one episode present in list

--- njpw-downloader/src/components/EpisodeSelectList.vue
+++ njpw-downloader/src/components/EpisodeSelectList.vue
@@ -15,12 +15,7 @@ const episode = computed({
 </script>
 
 <template>
-  <div v-if="episodes.length == 1">
-    <span class="ep-title">{{ ep.title }}</span>
-    -
-    <span class="ep-air-date">{{ ep.air_date }}</span>
-  </div>
-  <select v-model="episode" class="form-select" v-else>
+  <select v-model="episode" class="form-select">
     <option value="" disabled></option>
     <option v-for="ep in episodes" :key="ep.id" :value="ep.id">
       {{ ep.title }} - {{ ep.air_date }}
